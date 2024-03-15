e1d5431b951e3ab08ead5ac56ff4080b12259cd8
alexandra toloza rios <toloza.ale1992@gmail.com>
2024-03-15 11:26:11 -0300

    desafio Evaluado

--- Apoyo Desafío - Traer Post/assets/js/app.js
+++ Apoyo Desafío - Traer Post/assets/js/app.js
@@ -6,14 +6,12 @@ async function Traerdatos() {
        
         const posts = await response.json();
 
-        // Limpiamos la lista antes de agregar los nuevos posts
         const postData = document.getElementById('postData');
         postData.innerHTML = '';
 
-        // Iteramos sobre los posts y los agregamos a la lista
         posts.forEach(post => {
             const listItem = document.createElement('li');
-            listItem.textContent = post.title; // Mostramos solo el título del post
+            listItem.textContent = post.title; 
             postData.appendChild(listItem);
         });
     } catch (error) {
@@ -22,5 +20,5 @@ async function Traerdatos() {
     }
 }
 
-// Manejador de evento para el botón de obtención de posts
+
 btnPost.addEventListener('click', Traerdatos);
